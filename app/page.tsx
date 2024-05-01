22e9dd8bde8374e9c0be73873c439271c49baccd
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-05-01 20:25:09 -0300

    feat: ui dos modulos e player

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { Provider as ReduxProvider } from "react-redux";
-import AddTodo from "@/components/AddTodo";
-import TodoList from "@/components/TodoList";
+
 import Image from "next/image";
 import { store } from "@/store";
+import Player from "@/components/Player";
 
 export default function Home() {
   return (
     <ReduxProvider store={store}>
-      <TodoList /> <AddTodo />
+      <Player />
     </ReduxProvider>
   );
 }
--- store/index.ts
+++ store/index.ts
@@ -1,27 +1,9 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { UseSelector, TypedUseSelectorHook, useSelector } from "react-redux";
 
-const todoSlide = createSlice({
-  name: "todo",
-  initialState: [
-    "Estudar redux toolkit",
-    "Estudar next.js",
-    "estudar typescript",
-  ],
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSlide.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todoSlide.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
