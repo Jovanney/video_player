6d2e52ce961ba1fba920ac4295cdc74e847d327d
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-05-01 21:02:06 -0300

    feat: accordion

--- components/Module.tsx
+++ components/Module.tsx
@@ -1,3 +1,10 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 import { ChevronDown, Video } from "lucide-react";
 import React from "react";
 import Lesson from "./Lesson";
@@ -14,29 +21,28 @@ export default function Module({
   moduleIndex,
 }: ModuleProps) {
   return (
-    <div>
-      <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
-        <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
-          {moduleIndex + 1}
-        </div>
+    <Accordion type="single" collapsible>
+      <AccordionItem value={moduleIndex.toString()}>
+        <AccordionTrigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+          <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
+            {moduleIndex + 1}
+          </div>
 
-        <div className="flex flex-col gap-1 text-left">
-          <strong>{title}</strong>
-          <span className="text-sm text-zinc-400 ">
-            {amountOfLessons} aulas
-          </span>
-        </div>
-
-        <ChevronDown size={20} className="ml-auto text-zinc-400" />
-      </button>
-
-      <nav className="relative flex flex-col gap-4 p-6">
-        <Lesson title="Desvendando o Redux" duration="09:28" />
-        <Lesson title="Desvendando o Redux" duration="09:28" />
-        <Lesson title="Desvendando o Redux" duration="09:28" />
-        <Lesson title="Desvendando o Redux" duration="09:28" />
-        <Lesson title="Desvendando o Redux" duration="09:28" />
-      </nav>
-    </div>
+          <div className="flex flex-col gap-1 text-left">
+            <strong>{title}</strong>
+            <span className="text-sm text-zinc-400 ">
+              {amountOfLessons} aulas
+            </span>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Desvendando o Redux" duration="09:28" />
+          <Lesson title="Desvendando o Redux" duration="09:28" />
+          <Lesson title="Desvendando o Redux" duration="09:28" />
+          <Lesson title="Desvendando o Redux" duration="09:28" />
+          <Lesson title="Desvendando o Redux" duration="09:28" />
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
